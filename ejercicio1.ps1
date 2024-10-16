050049bfcdce320408dccadd399e34c03b052765
ANAHI <anahipa1006@gmail.com>
2024-10-16 13:38:32 +0200

    Creado el ejecicio1

---
+++ ejercicio1.ps1
@@ -0,0 +1,4 @@
+﻿New-Item -Path "C:\" -Name "prueba" -ItemType "directory"
+Set-Location -Path C:\prueba
+New-Item -Path . -Name "hola.txt" -ItemType "file"
+Copy-Item -Path "C:\prueba\hola.txt" -Destination "C:\prueba\hola.bak"
